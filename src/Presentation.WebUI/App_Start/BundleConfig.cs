247a52bb8cd7b03d608a1bcc8cb225e9f939f55b
ddemchenko <luganskman@gmail.com>
2013-09-13 10:51:42 -0700

    Restructuring files and folders to meet common practices during development with AngularJS

--- src/Presentation.WebUI/App_Start/BundleConfig.cs
+++ src/Presentation.WebUI/App_Start/BundleConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Web;
 using System.Web.Optimization;
 
 namespace Presentation.WebUI
@@ -8,20 +10,22 @@ namespace Presentation.WebUI
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            Contract.Requires<ArgumentNullException>(bundles != null);
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/vendor/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/vendor/jquery/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/vendor/jquery/jquery.unobtrusive*",
+                        "~/Scripts/vendor/jquery/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/vendor/bootstrap/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
